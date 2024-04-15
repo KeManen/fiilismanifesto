4e5c287f4bcbc4a1efaf24a768af8678e04f4fc6
KeManen <dev@kema.fi>
2024-04-16 01:28:11 +0300

    Init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<h1>Manifesto for Fiilis Software Development</h1>
+
+<p>We are uncovering better fiilis of developing software by doing it and helping others do it. Through this work we have come to value:</p>
+
+<p><b>Good Fiilis</b> over Individuals and interactions</p>
+<p><b>Good Fiilis</b> over Working Product</p>
+<p><b>Good Fiilis</b> over Customer Collaboration</p>
+<p><b>Good Fiilis</b> over Responding to change</p>
+
+<p>That is, while there is value in the items on the right, we value the Fiilis on the left more.</p>
---
+++ src/routes/principles/+page.svelte
@@ -0,0 +1,33 @@
+<h1><b>Principles behind the Fiilis Manifesto</b></h1>
+<h2>We follow these principles:</h2>
+
+<p>Our top priority is to have good Fiilis with continuous delivery of valuable software.</p>
+
+<p>We welcome changing requirements, even late in development, 
+as they provide opportunities to infuse more Fiilis into our products.</p>
+
+<p>We deliver working software frequently when it ready for the maximum Fiilis.</p>
+
+<p>Developers must work freely with Fiilis not impeded by Business people.</p>
+
+<p>We believe in empowering motivated individuals within an environment designed to boost Fiilis, 
+ providing them with the support they need to spread positivity and Fiilis.</p>
+
+<p>The most efficient and effective method of
+conveying information to and within a development
+team is face-to-face conversation.</p>
+
+<p>Fiilis is the primary measure of progress in software development.</p>
+
+<p>Fiilis processes promote sustainable development.
+The sponsors, developers, and users should be able enjoy the process indefinitely.</p>
+
+<p>Continuous attention to technical excellence and good design is essential for maintaining and enhancing the Fiilis experienced by all stakeholders.</p>
+
+<p>Simplicity is crucial for maximizing Fiilis in our work, as it allows us to focus on delivering what truly brings joy and satisfaction.</p>
+
+<p>The best architectures, requirements, and designs
+emerge from Fiilis.</p>
+
+<p>At regular intervals, the team reflects on how
+to enhance the Fiilis by tuning and adjusting behavior to amplify positivity and satisfaction.</p>
