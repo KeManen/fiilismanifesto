b35edc1e4706d01904976a53c16000a934f06edd
KeManen <dev@kema.fi>
2024-04-16 03:05:29 +0300

    Removed deploy and prettier

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,42 @@
 <html lang="en">
-<head>
-<title>Manifesto for Fiilis Software Development</title>
-<meta content="text/html: charset=utf-8" />
-</head>
-<body>
-<h1>Manifesto for Fiilis Software Development</h1>
-<br><br><br>
-<p>We are uncovering better fiilis of developing software by doing it and helping others do it. Through this work we have come to value:</p>
-<br><br>
+	<head>
+		<title>Manifesto for Fiilis Software Development</title>
+		<meta content="text/html: charset=utf-8" />
+	</head>
+	<body>
+		<h1>Manifesto for Fiilis Software Development</h1>
+		<br /><br /><br />
+		<p>
+			We are uncovering better fiilis of developing software by doing it and helping others do it.
+			Through this work we have come to value:
+		</p>
+		<br /><br />
 
-<p><b>Good Fiilis</b> over Individuals and interactions</p>
-<p><b>Good Fiilis</b> over Working Product</p>
-<p><b>Good Fiilis</b> over Customer Collaboration</p>
-<p><b>Good Fiilis</b> over Responding to change</p>
-<br><br>
+		<p><b>Good Fiilis</b> over Individuals and interactions</p>
+		<p><b>Good Fiilis</b> over Working Product</p>
+		<p><b>Good Fiilis</b> over Customer Collaboration</p>
+		<p><b>Good Fiilis</b> over Responding to change</p>
+		<br /><br />
 
-<p>That is, while there is value in the items on the right, we value the Fiilis on the left more.</p>
-<br><br>
+		<p>
+			That is, while there is value in the items on the right, we value the Fiilis on the left more.
+		</p>
+		<br /><br />
 
-<a href="/principles/">Twelve Principles of Fiilis Software</a>
+		<a href="/principles/">Twelve Principles of Fiilis Software</a>
+	</body>
 
-</body>
+	<style>
+		p {
+			text-align: justify;
+		}
 
-<style>
-p {
-    text-align: justify
-}
+		h1 {
+			text-align: center;
+		}
 
-h1 {
-    text-align: center
-}
-
-h2 {
-    text-align: center
-}   
-</style>
+		h2 {
+			text-align: center;
+		}
+	</style>
 </html>
--- src/routes/principles/+page.svelte
+++ src/routes/principles/+page.svelte
@@ -1,65 +1,75 @@
 <html lang="en">
-<head>
-<title>Principles behind the Fiilis Manifesto</title>
-<meta content="text/html: charset=utf-8" />
-</head>
-<body>
+	<head>
+		<title>Principles behind the Fiilis Manifesto</title>
+		<meta content="text/html: charset=utf-8" />
+	</head>
+	<body>
+		<h1><b>Principles behind the Fiilis Manifesto</b></h1>
+		<br /><br /><br />
+		<h2>We follow these principles:</h2>
+		<br /><br />
 
-<h1><b>Principles behind the Fiilis Manifesto</b></h1>
-<br><br><br>
-<h2>We follow these principles:</h2>
-<br><br>
+		<p>Our top priority is to have good Fiilis with continuous delivery of valuable software.</p>
 
-<p>Our top priority is to have good Fiilis with continuous delivery of valuable software.</p>
+		<p>
+			We welcome changing requirements, even late in development, as they provide opportunities to
+			infuse more Fiilis into our products.
+		</p>
 
-<p>We welcome changing requirements, even late in development, 
-as they provide opportunities to infuse more Fiilis into our products.</p>
+		<p>We deliver working software frequently when it ready for the maximum Fiilis.</p>
 
-<p>We deliver working software frequently when it ready for the maximum Fiilis.</p>
+		<p>Developers must work freely with Fiilis not impeded by Business people.</p>
 
-<p>Developers must work freely with Fiilis not impeded by Business people.</p>
+		<p>
+			We believe in empowering motivated individuals within an environment designed to boost Fiilis,
+			providing them with the support they need to spread positivity and Fiilis.
+		</p>
 
-<p>We believe in empowering motivated individuals within an environment designed to boost Fiilis, 
- providing them with the support they need to spread positivity and Fiilis.</p>
+		<p>
+			The most efficient and effective method of conveying information to and within a development
+			team is face-to-face conversation.
+		</p>
 
-<p>The most efficient and effective method of
-conveying information to and within a development
-team is face-to-face conversation.</p>
+		<p>Fiilis is the primary measure of progress in software development.</p>
 
-<p>Fiilis is the primary measure of progress in software development.</p>
+		<p>
+			Fiilis processes promote sustainable development. The sponsors, developers, and users should
+			be able enjoy the process indefinitely.
+		</p>
 
-<p>Fiilis processes promote sustainable development.
-The sponsors, developers, and users should be able enjoy the process indefinitely.</p>
+		<p>
+			Continuous attention to technical excellence and good design is essential for maintaining and
+			enhancing the Fiilis experienced by all stakeholders.
+		</p>
 
-<p>Continuous attention to technical excellence and good design is essential for maintaining and enhancing the Fiilis experienced by all stakeholders.</p>
+		<p>
+			Simplicity is crucial for maximizing Fiilis in our work, as it allows us to focus on
+			delivering what truly brings joy and satisfaction.
+		</p>
 
-<p>Simplicity is crucial for maximizing Fiilis in our work, as it allows us to focus on delivering what truly brings joy and satisfaction.</p>
+		<p>The best architectures, requirements, and designs emerge from Fiilis.</p>
 
-<p>The best architectures, requirements, and designs
-emerge from Fiilis.</p>
+		<p>
+			At regular intervals, the team reflects on how to enhance the Fiilis by tuning and adjusting
+			behavior to amplify positivity and satisfaction.
+		</p>
 
-<p>At regular intervals, the team reflects on how
-to enhance the Fiilis by tuning and adjusting behavior to amplify positivity and satisfaction.</p>
+		<br /><br />
 
-<br><br>
+		<a href="/">Return to Manifesto</a>
+	</body>
 
-<a href="/">Return to Manifesto</a>
-</body>
+	<style>
+		p {
+			text-align: justify;
+		}
 
-<style>
-p {
-    text-align: justify
-}
+		h1 {
+			text-align: center;
+		}
 
-h1 {
-    text-align: center
-}
-
-h2 {
-    text-align: center
-}
-
-
-</style>
-
+		h2 {
+			text-align: center;
+		}
+	</style>
 </html>
